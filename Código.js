a48c479b6bca227ccb98d2881917800a47696288
LucasSantos-png <lucas.santos2717@etec.sp.gov.br>
2026-09-18 17:49:30 -0300

    Implementa armazenamento do Artigo científico na planilha

--- Código.js
+++ Código.js
@@ -14,12 +14,12 @@ const CONFIG = {
 }
 
 /*função que salva os dados na planilha
-  dados de entrada: data/hora (gerado automaticamente), professor, titulo*/
+  dados de entrada: data/hora (gerado automaticamente), avaliador, titulo*/
 
-function salvarTeste(professor, titulo) {
+function salvarTeste(avaliador, titulo) {
 
   //pega planilha por id
-  const planilha = SpreadsheetApp.openById.CONFIG.PLANILHA_ID;
+  const planilha = SpreadsheetApp.openById(CONFIG.PLANILHA_ID);
   
   //seleciona a página da planilha
   const aba = planilha.getSheetByName('TESTE');
@@ -27,7 +27,7 @@ function salvarTeste(professor, titulo) {
   //adiciona linha com dados inseridos no banco
   aba.appendRow([
     new Date(),
-    professor,
+    avaliador,
     titulo
   ]);
 
@@ -37,8 +37,8 @@ function salvarTeste(professor, titulo) {
 
 //função que gera um arquivo txt com os dados informados
 //saída: txt com Nome e Titulo do TCC
-function criarArquivoTeste(professor, titulo) {
-  const conteudo = 'Professor: ' + professor + '\n' + 'Título do TCC: ' + titulo;
+function criarArquivoTeste(avaliador, titulo) {
+  const conteudo = 'Avaliador: ' + avaliador + '\n' + 'Título do TCC: ' + titulo;
 
   DriveApp.createFile('Teste_Thesia.txt', conteudo);
 
@@ -46,13 +46,13 @@ function criarArquivoTeste(professor, titulo) {
 }
 
 //função que gera cópia do modelo de doc
-function gerarDocumentoTeste(professor, titulo) {
+function gerarDocumentoTeste(avaliador, titulo) {
 
   //id do arquivo de documento modelo a ser preenchido
   const arquivoModelo = DriveApp.getFileById(CONFIG.MODELO_DOC_ID);
 
   //faz uma cópia e altera o título
-  const copia = arquivoModelo.makeCopy(titulo + ' - ' + professor);
+  const copia = arquivoModelo.makeCopy(titulo + ' - ' + avaliador);
 
   //abre o doc por id da cópia
   const documento = DocumentApp.openById(copia.getId());
@@ -61,7 +61,7 @@ function gerarDocumentoTeste(professor, titulo) {
   const corpo = documento.getBody();
 
   //substitui os dados digitados no documento
-  corpo.replaceText('<<PROFESSOR>>', professor);
+  corpo.replaceText('<<AVALIADOR>>', avaliador);
 
   corpo.replaceText('<<TITULO>>', titulo);
 
@@ -84,22 +84,57 @@ function salvarAvaliacaoTeste(dados)
 {
   const planilha = SpreadsheetApp.openById(CONFIG.PLANILHA_ID);
 
-  const aba = planilha.getSheetByName('TESTE');
+  const abaAvaliacoes = planilha.getSheetByName("AVALIACOES");
+  const abaArtigo = planilha.getSheetByName("ARTIGO");
 
-  //calcula novamente para jogar na planilha
-  const final1 = (dados.artigo * 0.3) + (dados.relatorio * 0.3) + (dados.oral1 * 0.4);
-  const final2 = (dados.artigo * 0.3) + (dados.relatorio * 0.3) + (dados.oral2 * 0.4);
-  const final3 = (dados.artigo * 0.3) + (dados.relatorio * 0.3) + (dados.oral3 * 0.4);
-  const final4 = (dados.artigo * 0.3) + (dados.relatorio * 0.3) + (dados.oral4 * 0.4);
+  //cria um id unico para a avaliação atual
+  const idAvaliacao = Utilities.getUuid();
 
-  //adiciona uma linha com as informações preenchidas na planilha
-  aba.appendRow([
-    new Date(), dados.professor, dados.titulo, dados.artigo, dados.relatorio, dados.aluno1, 
-    dados.oral1, final1, dados.aluno2, dados.oral2, final2, dados.aluno3, dados.oral3, final3, 
-    dados.aluno4, dados.oral4, final4
+  //data/hora para os registros
+  const dataHora = new Date();
+
+  //pesos das notas
+  const pesoArtigo = 0.3;
+  const pesoRelatorio = 0.3;
+  const pesoOral = 0.4;
+
+
+  //soma dos critérios de Artigo e
+  const totalArtigo = dados.artigo1 + dados.artigo2 + dados.artigo3
+  + dados.artigo4 + dados.artigo5 + dados.artigo6 + dados.artigo7;
+
+
+  //soma as notas para gerar nota final
+  const final1 = (totalArtigo * pesoArtigo) + (dados.relatorio * pesoRelatorio) + (dados.oral1 * pesoOral);
+  const final2 = (totalArtigo * pesoArtigo) + (dados.relatorio * pesoRelatorio) + (dados.oral2 * pesoOral);
+  const final3 = (totalArtigo * pesoArtigo) + (dados.relatorio * pesoRelatorio) + (dados.oral3 * pesoOral);
+  const final4 = (totalArtigo * pesoArtigo) + (dados.relatorio * pesoRelatorio) + (dados.oral4 * pesoOral);
+
+  //salva o resumo na aba AVALIACOES
+  abaAvaliacoes.appendRow([
+    idAvaliacao, dataHora, dados.avaliador, dados.titulo, dados.aluno1, final1, dados.aluno2, final2, dados.aluno3, final3, dados.aluno4, final4
   ]);
 
+  // salva os critérios do artigo na aba ARTIGO
+  abaArtigo.appendRow([
+    idAvaliacao, dados.artigo1, dados.artigo2, dados.artigo3, dados.artigo4, dados.artigo5, dados.artigo6, dados.artigo7, totalArtigo
+  ]);
+
+  //devolve os resultados para o html
   return {
-    aluno1: dados.aluno1, final1: final1, aluno2: dados.aluno2, final2: final2, aluno3: dados.aluno3, final3: final3, aluno4: dados.aluno4, final4: final4
+    idAvaliacao: idAvaliacao,
+    totalArtigo: totalArtigo,
+
+    aluno1: dados.aluno1,
+    final1: final1,
+
+    aluno2: dados.aluno2,
+    final2: final2,
+
+    aluno3: dados.aluno3,
+    final3: final3,
+
+    aluno4: dados.aluno4,
+    final4: final4
   };
 }
