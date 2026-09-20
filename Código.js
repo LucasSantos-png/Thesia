7ea387f68be1d8f99415c4dad91ebca5a962a7b7
LucasSantos-png <lucas.santos2717@etec.sp.gov.br>
2026-09-19 23:38:37 -0300

    Adicao de mais comentarios explicativos

--- Código.js
+++ Código.js
@@ -1,5 +1,4 @@
 /*função que exibe o site*/
-
 function doGet() 
 {
   return HtmlService.createHtmlOutputFromFile('index')
@@ -15,7 +14,7 @@ const CONFIG =
   MODELO_DOC_ID: propriedades.getProperty('MODELO_DOC_ID')
 }
 
-
+//função que salva a avaliação na planilha do drive
 function salvarAvaliacao(dados)
 {
   //abre a planilha por id
@@ -47,21 +46,24 @@ function salvarAvaliacao(dados)
   const totalRelatorio = dados.relatorio1 + dados.relatorio2 + dados.relatorio3
   + dados.relatorio4 + dados.relatorio5;
 
-  //soma dos critérios de Oratória por aluno
+  //soma dos critérios de Oratória aluno 1
   const totalOral1 = dados.oral1aluno1 + dados.oral2aluno1 + dados.oral3aluno1
   + dados.oral4aluno1 + dados.oral5aluno1;
 
+  //soma dos critérios de Oratória aluno 2
   const totalOral2 = dados.oral1aluno2 + dados.oral2aluno2 + dados.oral3aluno2
   + dados.oral4aluno2 + dados.oral5aluno2;
 
+  //soma dos critérios de Oratória aluno 3
   const totalOral3 = dados.oral1aluno3 + dados.oral2aluno3 + dados.oral3aluno3
   + dados.oral4aluno3 + dados.oral5aluno3;
 
+  //soma dos critérios de Oratória aluno 4
   const totalOral4 = dados.oral1aluno4 + dados.oral2aluno4 + dados.oral3aluno4
   + dados.oral4aluno4 + dados.oral5aluno4;
 
 
-  //soma as notas com seus pesos para gerar nota final
+  //soma as notas com seus pesos para gerar nota final de cada aluno
   const final1 = (totalArtigo * pesoArtigo) + (totalRelatorio * pesoRelatorio) + (totalOral1 * pesoOral);
   const final2 = (totalArtigo * pesoArtigo) + (totalRelatorio * pesoRelatorio) + (totalOral2 * pesoOral);
   const final3 = (totalArtigo * pesoArtigo) + (totalRelatorio * pesoRelatorio) + (totalOral3 * pesoOral);
@@ -82,23 +84,25 @@ function salvarAvaliacao(dados)
     idAvaliacao, dados.relatorio1, dados.relatorio2, dados.relatorio3, dados.relatorio4, dados.relatorio5, totalRelatorio
   ]);
 
-  //salva os critérios de oratória na aba ORAL
+  //salva os critérios de oratória na aba ORAL - linha do aluno1
   abaOral.appendRow([
     idAvaliacao, 1, dados.aluno1, dados.oral1aluno1, dados.oral2aluno1, dados.oral3aluno1, dados.oral4aluno1, dados.oral5aluno1, totalOral1
   ]);
 
+  //salva os critérios de oratória na aba ORAL - linha do aluno2
   abaOral.appendRow ([
     idAvaliacao, 2, dados.aluno2, dados.oral1aluno2, dados.oral2aluno2, dados.oral3aluno2, dados.oral4aluno2, dados.oral5aluno2, totalOral2
   ]);
 
+  //salva os critérios de oratória na aba ORAL - linha do aluno3
   abaOral.appendRow ([
     idAvaliacao, 3, dados.aluno3, dados.oral1aluno3, dados.oral2aluno3, dados.oral3aluno3, dados.oral4aluno3, dados.oral5aluno3, totalOral3
   ]);
 
+  //salva os critérios de oratória na aba ORAL - linha do aluno4
   abaOral.appendRow ([
     idAvaliacao, 4, dados.aluno4, dados.oral1aluno4, dados.oral2aluno4, dados.oral3aluno4, dados.oral4aluno4, dados.oral5aluno4, totalOral4
   ]);
-
 
   //devolve os resultados para o html
   return {
